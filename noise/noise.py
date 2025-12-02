a3c6251ca40318cb017c4844b637bf54126c9113
Peter Wegmann <peter.wegmann@yahoo.de>
2025-12-02 15:12:42 +0000

    Fix addition of depolarization1 error

--- noise/noise.py
+++ noise/noise.py
@@ -61,10 +61,7 @@ class NoiseModel:
     
     def add_qubit_error(self, circuit: stim.Circuit, qubits: List[stim.GateTarget], gate_duration: float) -> None:
         # https://arxiv.org/pdf/1404.3747
-        circuit.append_operation("Depolarize1", qubits, self.idle)
-        return
-        if self.backend == None:
-            circuit.append_operation("Depolarize1", qubits, self.idle)
+
         if self.backend == None or self.idle != 0:
             return
         for qubit in qubits:
@@ -210,7 +207,7 @@ class NoiseModel:
             else:
                 p = self.measure
             pre.append_operation("Z_ERROR" if op.name.endswith("X") else "X_ERROR", targets, p)
-            self.add_qubit_error(post, targets, self.get_gate_time(op))
+            #self.add_qubit_error(post, targets, self.get_gate_time(op))
         elif op.name == "MPP":
             # Our circuits never contain MPP after translations
             assert len(targets) % 3 == 0 and all(t.is_combiner for t in targets[1::3]), repr(op)
